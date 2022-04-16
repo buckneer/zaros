cc4b478106e662010dc0fa8d491b1f61f4a1246c
Miftari Simel <miftarisimel@gmail.com>
2022-04-16 19:01:07 +0200

    Project setup

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footer.scss";
+
+
+export default function Footer() {
+    return (
+        <div className="Footer">
+            <h1>Footer</h1>
+        </div>
+    )
+}
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,9 @@
+import "./Navigation.scss";
+
+export default function Navigation() {
+    return (
+        <div className="Navigation">
+            <h1>Navigation</h1>
+        </div>
+    )
+}
